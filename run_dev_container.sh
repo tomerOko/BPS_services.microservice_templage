56f8cf64469c491498cee8bb28d43da33b5cd49d
tomerOko <tomeroko2@gmail.com>
2022-01-19 19:08:44 +0200

    prod is ready

--- run_dev_container.sh
+++ run_dev_container.sh
@@ -1,12 +1,12 @@
 
 sudo chown -R $USER:$(id -gn $USER) ./* # this gives the host OS (and by that to the code editor u using) permissions to eddit the content of this folder
 
-sudo docker build -t microservice_template_dev.image . # build the image and names it 'microservice_template_dev.image'
+sudo docker build -t microservice_template_dev.image ./development # build the image and names it 'microservice_template_dev.image'
 
 docker rm -f microservice_template_dev.container &>/dev/null # remove the container in case its already exist. rm: remove, -f: force - in case the older version of this container is already exist and runing
 
 docker run -itd --name microservice_template_dev.container -p 30001:3000 -v ${PWD}:/app microservice_template_dev.image 
-#
+# ↑↑ the comments below are for the command above ↑↑#
 #  docker run -itd : run the container seperatly from the cli. if the cli is closed the container keep on runing
 #  --name microservice_template_dev.container : container's name
 #  -p 30001:3000 : connect host's port 30001 to container's port 3000 (-p <host port>:<container port>)
@@ -16,7 +16,10 @@ docker run -itd --name microservice_template_dev.container -p 30001:3000 -v ${PW
 #  #bash 
 #
 
-docker exec -it microservice_template_dev.container bash -c 'npm run dev'  # connect to a cli inside the container
+docker exec -it microservice_template_dev.container bash -c 'cd development && npm run dev'  # connect to a cli inside the container and run: 'npm run dev'
+# docker exec -it microservice_template_dev.container bash  # connect to a cli inside the container
+# if, insted of the two options above, u would like to open a vs-code remote container, see https://code.visualstudio.com/docs/remote/attach-container
+
 #
 # docker exec : docker execute
 # -it : 'i' means interactive, if u run a command and close the shell, the procces will keep on runing. 't' - i donk realy know, somthing with the output and 'TTY'
